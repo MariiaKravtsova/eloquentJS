b999e2564f70280aa1ca963458c775e7e56d1edd
MariiaKravtsova <kravtsova.mariia@gmail.com>
2017-03-12 17:46:18 -0400

    Add server, file streams, buffers, pipe

---
+++ introNode/app.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var fs = require('fs');
+
+
+/*
+var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
+
+myReadStream.on('data', function(chunk) {
+  console.log('new chunk received:');
+  console.log(chunk);
+  myWriteStream.write(chunk);
+});
+*/
+
+var server = http.createServer(function(req, res) {
+  console.log('request made: ' + req.url);
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+  myReadStream.pipe(res);
+});
+
+server.listen(3000, '127.0.0.1');
+console.log('I am listening on port 3000');
