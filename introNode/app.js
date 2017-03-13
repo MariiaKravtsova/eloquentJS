913d98fd19c156a314dcfe8ba814ad900ff5b20f
MariiaKravtsova <kravtsova.mariia@gmail.com>
2017-03-12 23:57:34 -0400

    Make views using express, and use get/post requests.

--- introNode/app.js
+++ introNode/app.js
@@ -1,56 +1,27 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
+var bodyParser = require('body-parser')
+var app = express();
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
 
-/*
-var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
-var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
-
-myReadStream.on('data', function(chunk) {
-  console.log('new chunk received:');
-  console.log(chunk);
-  myWriteStream.write(chunk);
-});
-*/
-
-/*
-var server = http.createServer(function(req, res) {
-  console.log('request made: ' + req.url);
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  var myObj = {
-    name: 'Donatelo',
-    job: 'ninja',
-    age: 12
-  };
-  res.end(JSON.stringify(myObj))
-});
-*/
-
-var server = http.createServer(function(req, res) {
-  console.log('request made: ' + req.url);
-  if (req.url === '/home' || req.url === '/') {
-
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/index.html').pipe(res);
-
-  } else if (req.url === '/contact') {
-
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/contact.html').pipe(res);
-
-  } else if (req.url === '/api/ninjas') {
-
-    var ninjas = [{name: 'Donatelo', age: 12}, {name: 'Michelangelo', age: 14}];
-    res.writeHead(200, {'Content-Type': 'application/json'})
-    res.end(JSON.stringify(ninjas))
-
-  } else {
-
-    res.writeHead(404, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/404.html').pipe(res);
-    
-  }
+app.get('/', function(req, res) {
+  res.render('index');
 });
 
-server.listen(3000, '127.0.0.1');
-console.log('I am listening on port 3000');
+app.get('/contact', function(req, res) {
+  res.render('contact', {qs: req.query});
+});
+
+app.post('/contact', urlencodedParser, function(req, res) {
+  console.log(req.body)
+  res.render('contact-success', {data: req.body});
+});
+
+app.get('/profile/:name', function(req, res) {
+  var data = {age: 19, job: 'ninja', hobbies: ['eating', 'fishing', 'coding']};
+  res.render('profile', {person: req.params.name, data: data});
+});
+
+app.listen(3000);
