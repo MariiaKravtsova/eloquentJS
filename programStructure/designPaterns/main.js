227179a4b28c2138265da9cddfb81e4702412866
MariiaKravtsova <kravtsova.mariia@gmail.com>
2017-04-15 16:21:36 -0400

    Module and Singleton patterns

--- programStructure/designPaterns/main.js
+++ programStructure/designPaterns/main.js
@@ -1,7 +1,8 @@
 var Task = require('./task');
+var repo = require('./taskRepository');
 
-var task1 = new Task('eat pineapple');
-var task2 = new Task('drink water');
+var task1 = new Task(repo.get(1));
+var task2 = new Task({name: 'drink water'});
 
 task1.save();
 task2.save();
