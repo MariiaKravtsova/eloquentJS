2e358ac637c92e78f53ca4a6085da990f3386464
MariiaKravtsova <kravtsova.mariia@gmail.com>
2017-04-15 14:40:24 -0400

    Add style and finish calculateTotal math

---
+++ tips/README.md
@@ -0,0 +1,7 @@
+# Tips
+*purpose:* Play with Materialize CSS, DOM, and practice my understanding of this, let, const keywords. 
+
+Also, I need help tipping sometimes.
+
+## Screenshot
+![Tips Screenshot](https://raw.githubusercontent.com/MariiaKravtsova/eloquentJS/master/tips/screenshot.png "Tips Screenshot")
--- tips/script.js
+++ tips/script.js
@@ -1,11 +1,14 @@
 const amount = document.querySelector('.amount');
 const percent = document.getElementById('percent');
 const calculateButton = document.querySelector('[data-action="calculate"]');
-let total = document.getElementsByTagName('p');
+let tip = document.querySelector('.tip');
+let total = document.querySelector('.total');
 
 function calculateTotal() {
-    console.log(amount.value);
-
+    this.tip = parseFloat(amount.value) * parseFloat(percent.value);
+    this.total = parseFloat(amount.value) + this.tip;
+    total.innerText = this.total;
+    tip.innerText = this.tip;
 }
 
 calculateButton.addEventListener('click', calculateTotal);
