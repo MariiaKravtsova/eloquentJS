4c060408b2c677a10e253a9a99637a3e66c7f3a4
MariiaKravtsova <kravtsova.mariia@gmail.com>
2017-04-16 18:06:01 -0400

    Add scope and mutable object

--- mpj/higherOrder.js
+++ mpj/higherOrder.js
@@ -23,3 +23,34 @@ console.log('The dog names are ' + dogs[0].name + ' and ' + dogs[1].name);
 
 let ages = animals.reduce((sum, animal) => sum + animal.age, 0)
 console.log('Animals are ' + ages + ' years old.');
+
+const foo = 'bar';
+
+function bar() {
+    let foo = 'baz';
+    console.log(foo);
+    function baz(foo) {
+        console.log(foo);
+        foo = 'bam';
+        console.log(foo);
+        bam = 'yay';    
+        console.log(bam);
+    }
+    baz();
+}
+// console.log(bam);
+console.log(foo);
+
+bar();
+console.log(foo);
+
+const player = {
+    score: 1,
+    name: 'Maya'
+}
+// with mutation
+player.name = 'dogs';
+console.log(player);
+// witout mutation
+const newPlayer = Object.assign({}, player, {name: 'Maya'});
+console.log(newPlayer);
